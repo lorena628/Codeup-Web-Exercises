a4c6934ace3e9b1b0a72c2b86544cc40f750d0d7
lo v <lov@los-MacBook-Pro.local>
2016-05-02 09:39:26 -0500

    practice

---
+++ public/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<style>
+    body {
+        background-image: url(/img/airplanefan.jpeg);
+        background-size: cover;
+    }
+    footer { 
+    display: block;
+}
+
+</style>
+</head>
+<body>
+
+    <?php require_once 'header.php'; ?>
+<footer class="container">
+  <h5>Footer</h5>
+  <p>Footer information goes here</p>
+</footer>
+  
+
+</body>
+</html>
