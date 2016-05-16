9e0dda8a62113ec92a2435e614f6355be197d961
lo v <lov@los-MacBook-Pro.local>
2016-05-16 14:00:44 -0500

    added modifications with bindvalues

--- park_migration.php
+++ park_migration.php
@@ -13,9 +13,10 @@ $dbc->exec('DROP TABLE IF EXISTS national_parks');
 $query = 'CREATE TABLE national_parks (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    location VARCHAR(55),
-    date_established YEAR(4),
-    area_in_acres DOUBLE(10, 2),
+    location VARCHAR(55) NOT NULL,
+    date_established YEAR(4) NOT NULL,
+    area_in_acres DOUBLE(10, 2) NOT NULL,
+    description VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
 )';
 $dbc->exec($query);
--- park_seeder.php
+++ park_seeder.php
@@ -14,18 +14,29 @@ $dbc->exec('TRUNCATE national_parks');
 // $dbc->exec('DROP TABLE IF EXISTS national_parks');
 
 $listOfParks = [
-    ['name' => 'Acadia', 'location' => 'Maine', 'date_established' => '1919', 'area_in_acres' => '47389.67'],
-    ['name' => 'American Samoa', 'location' => 'American Samoa', 'date_established' => '1988', 'area_in_acres' => '9000.00'],
-    ['name' => 'Arches', 'location' => 'Utah', 'date_established' => '1929', 'area_in_acres' => '76518.98'],
-    ['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978', 'area_in_acres' => '242755.94'],
-    ['name' => 'Big Bend', 'location' => 'Texas', 'date_established' => '1944', 'area_in_acres' => '801163.21'],
-    ['name' => 'Biscayne', 'location' => 'Florida', 'date_established' => '1980', 'area_in_acres' => '172924.07'],
-    ['name' => 'Black Canyon of the Gunnison', 'location' => 'Colorado', 'date_established' => '1999', 'area_in_acres' => '32950.03'],
-    ['name' => 'Bryce Canyon', 'location' => 'Utah', 'date_established' => '1928', 'area_in_acres' => '35835.08'],
-    ['name' => 'Canyonlands', 'location' => 'Utah', 'date_established' => '1912', 'area_in_acres'  => '337597.83'],
-    ['name' => 'Capitol Reef', 'location' => 'Utah', 'date_established' => '1918', 'area_in_acres'  => '241904.26'],  
+    ['name' => 'Acadia', 'location' => 'Maine', 'date_established' => '1919', 'area_in_acres' => '47389.67', 'description' => 'description of acadia'],
+    ['name' => 'American Samoa', 'location' => 'American Samoa', 'date_established' => '1988', 'area_in_acres' => '9000.00', 'description' => 'description of the american samoa'],
+    ['name' => 'Arches', 'location' => 'Utah', 'date_established' => '1929', 'area_in_acres' => '76518.98', 'description' => 'description of Arches'],
+    ['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978', 'area_in_acres' => '242755.94', 'description' => 'desc. of the badlands'],
+    ['name' => 'Big Bend', 'location' => 'Texas', 'date_established' => '1944', 'area_in_acres' => '801163.21', 'description' => 'desc of big bend'],
+    ['name' => 'Biscayne', 'location' => 'Florida', 'date_established' => '1980', 'area_in_acres' => '172924.07', 'description' => 'desc of biscayne'],
+    ['name' => 'Black Canyon of Gunnison', 'location' => 'Colorado', 'date_established' => '1999', 'area_in_acres' => '32950.03', 'description' => 'desc.of black canyon of gunnison'],
+    ['name' => 'Bryce Canyon', 'location' => 'Utah', 'date_established' => '1928', 'area_in_acres' => '35835.08', 'description' => 'des of bryce canyon'],
+    ['name' => 'Canyonlands', 'location' => 'Utah', 'date_established' => '1912', 'area_in_acres'  => '337597.83', 'description' => 'des of canyonlands'],
+    ['name' => 'Capitol Reef', 'location' => 'Utah', 'date_established' => '1918', 'area_in_acres'  => '241904.26', 'description' => 'desc of capitol reef'],    
 ];
+
+
+$stmt = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)');
+
+
 foreach ($listOfParks as $parks) {
-    $query = "INSERT INTO national_parks (name, location, date_established, area_in_acres) VALUES ('{$parks['name']}','{$parks['location']}','{$parks['date_established']}','{$parks['area_in_acres']}')";
-$dbc->exec($query);
+    $stmt->bindValue(':name', $parks['name'], PDO::PARAM_STR);
+    $stmt->bindValue(':location',  $parks['location'],  PDO::PARAM_STR);
+    $stmt->bindValue(':date_established', $parks['date_established'], PDO::PARAM_INT);
+    $stmt->bindValue(':area_in_acres', $parks['area_in_acres'], PDO::PARAM_INT);
+    $stmt->bindValue(':description', $parks['description'], PDO::PARAM_STR);
+
+    // $query = "INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)";
+$stmt->execute();
 }
