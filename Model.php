d71c9b62e144aa38c2bf6fce9f7dc8a14bfb49f6
lo v <lov@los-MacBook-Pro.local>
2016-05-24 12:46:18 -0500

    created a model with user extending model

--- Model.php
+++ Model.php
@@ -1,32 +1,99 @@
 <?php
-class Model
+require_once "ad_lister_credentials.php";
+abstract class Model
 {
-    // Array to store our key/value data
-    private $attributes = array();
-    protected static $table;
+    /** @var PDO|null Connection to the database */
+    protected static $dbc = null;
 
-    // Magic setter to populate $attributes array
-    public function __set($name, $value)
+    /** @var array Database values for a single record. Array keys should be column names in the DB */
+    protected $attributes = array();
+
+    /**
+     * Constructor
+     *
+     * An instance of a class derived from Model represents a single record in the database.
+     *
+     * $param array $attributes Optional array of database values to initialize the model record with
+     */
+    public function __construct(array $attributes = array())
     {
-        // Set the $name key to hold $value in $attributes
-        $this -> attributes[$name] = $value;
+        self::dbConnect();
+
+        // @TODO: Initialize the $attributes property with the passed value
+        $this->attributes = $attributes;
     }
 
-    // Magic getter to retrieve values from $attributes
+    /**
+     * Connect to the DB
+     *
+     * This method should be called at the beginning of any function that needs to communicate with the database
+     */
+    protected static function dbConnect()
+    {
+        if (!self::$dbc) { 
+        // connct to database and checking if null...if we don't have a db connection make a new one
+        // tell PDO to throw exceptions on error 
+            self::$dbc = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME,DB_USER,DB_PASS);
+            self::$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+    }
+
+    /**
+     * Get a value from attributes based on its name
+     *
+     * @param string $name key for attributes array
+     *
+     * @return mixed|null value from the attributes array or null if it is undefined
+     */
     public function __get($name)
     {
-        // Check for existence of array key $name
-        if(array_key_exists($name, $this-> attributes))
-            return $this->attributes[$name];
+        // @TODO: Return the value from attributes for $name if it exists, else return null
+            return isset($this->attributes[$name]) ? $this->attributes[$name] : null;
+            // Check for existence of array key $name
+            // if(array_key_exists($name, $this-> attributes)){
+            //     return $this->attributes[$name];
+            //  } else {
+            //         return null;
+            // }
     }
 
-
-
-    public static function getTableName()
+    /**
+     * Set a new value for a key in attributes
+     *
+     * @param string $name  key for attributes array
+     * @param mixed  $value value to be saved in attributes array
+     */
+    public function __set($name, $value)
     {
-        // static users will come up but self keyword will refer to the model class or base class  static is child and self is the base or parent  class 
-        return static::$table;
+        // @TODO: Store name/value pair in attributes array
+          // Set the $name key to hold $value in $attributes
+        $this->attributes[$name] = $value;
     }
 
-}
+    /** Store the object in the database */
+    public function save()
+    {
+        // @TODO: Ensure there are values in the attributes array before attempting to save
+        // @TODO: Call the proper database method: if the `id` is set this is an update, else it is a insert
+        if(!empty($this->attributes)){
+            if(isset($this->attributes['id'])){
+                $this->update(); // update($this->attributes['id']);
+            }else{
+                $this->insert();
+            }
+        }
+    }
 
+    /**
+     * Insert new entry into database
+     *
+     * NOTE: Because this method is abstract, any child class MUST have it defined.
+     */
+    protected abstract function insert();
+    /**
+     * Update existing entry in database
+     *
+     * NOTE: Because this method is abstract, any child class MUST have it defined.
+     */
+    protected abstract function update();
+}
--- User.php
+++ User.php
@@ -1,8 +1,96 @@
 <?php
-require_once 'Model.php';
+
+// __DIR__ is a *magic constant* with the directory path containing this file.
+// This allows us to correctly require_once Model.php, no matter where this file is being required from.
+require_once __DIR__ . '/Model.php';
 
 class User extends Model
 {
-    protected static $table = 'users';
+    /** Insert a new entry into the database */
+    protected function insert()
+    { 
+        // @TODO: Use prepared statements to ensure data security
+        // @TODO: You will need to iterate through all the attributes to build the prepared query
+        // @TODO: After the insert, add the id back to the attributes array
+        //        so the object properly represents a DB record
+        // self::dbConnect();
+        $query = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $stmt =  self::$dbc->prepare($query);
+        //binding
+        $stmt->bindValue(':name', $this->attributes['name'], PDO::PARAM_STR);
+        $stmt->bindValue(':email',  $this->attributes['email'],  PDO::PARAM_STR);
+        $stmt->bindValue(':password', $this->attributes['password'],  PDO::PARAM_STR);
+            $result = $stmt->execute();
+
+        if($result) {
+            $this->attributes['id'] = self::$dbc->lastInsertId();
+        }
+    }
+
+    /** Update existing entry in the database */
+    protected function update()
+    {
+        // @TODO: Use prepared statements to ensure data security
+        // @TODO: You will need to iterate through all the attributes to build the prepared query
+        // self::dbConnect();
+        $query = "UPDATE users (name, email, password, id) VALUES (:name, :email, :password, :id)";
+        $stmt =  self::$dbc->prepare($query);
+        //binding
+        $stmt->bindValue(':name', $this->attributes['name'], PDO::PARAM_STR);
+        $stmt->bindValue(':email',  $this->attributes['email'],  PDO::PARAM_STR);
+        $stmt->bindValue(':password',  $this->attributes['password'],  PDO::PARAM_STR);
+        $stmt->bindValue(':id',  $this->attributes['id'],  PDO::PARAM_INT);
+            //executing
+        $result = $stmt->execute();
+    }
+
+    /**
+     * Find a single record in the DB based on its id
+     *
+     * @param int $id id of the user entry in the database
+     *
+     * @return User An instance of the User class with attributes array set to values from the database
+     */
+    public static function find($id)
+    {
+        // Get connection to the database
+        // @TODO: Create select statement using prepared statements
+        // @TODO: Store the result in a variable named $result
+        self::dbConnect();
+        $stmt = self::$dbc->prepare('SELECT * FROM users WHERE id = :id');  
+        $stmt->bindValue(':id',  $id,  PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // The following code will set the attributes on the calling object based on the result variable's contents
+        $instance = null;
+        if ($result) {
+            $instance = new static($result);
+            // $instance->attributes = $result;
+            // echo "inside of find if";
+        }
+        return $instance;
+    }
+
+    /**
+     * Find all records in a table
+     *
+     * @return User[] Array of instances of the User class with attributes set to values from database
+     */
+    public static function all()
+    {
+        // @TODO: Learning from the find method, return all the matching records
+        self::dbConnect();
+        $stmt = self::$dbc->prepare("SELECT * FROM users");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+    // public function delete()
+    // {
+    //     self::dbConnect();
+    //     $stmt = self::$dbc->prepare('DELETE FROM users WHERE id = id');
+    //     $stmt->bindValue(':id', $this->attributes['id'], PDO::PARAM_INT)
+    //     $stmt->execute();
+    // }
 }
-echo User::getTableName() . PHP_EOL;
